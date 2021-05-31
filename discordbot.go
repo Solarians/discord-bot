92edcb95e68b6c353c6b08021d7a85ed42ae69c2
OswyChow <chowoswy@gmail.com>
2021-05-31 10:35:57 +0200

    Start development

---
+++ discordbot.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/lus/dgc"
+)
+
+const (
+	token = "ODQ4ODAxMzYzMzY5Nzg3NDQ0.YLR53g.mdfjzO1DyFow8d5D_uDdP8rcomQ"
+)
+
+func startBot() (*discordgo.Session, error) {
+	log.Println("!-STARTING BOT-!")
+	s, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return nil, fmt.Errorf("Invalid bot parameters: %v", err)
+	}
+
+	err = s.Open()
+	if err != nil {
+		return nil, fmt.Errorf("Error connecting: %v", err)
+	}
+	log.Println("!-BOT STARTED-!")
+	return s, nil
+}
+
+func registerCommands(r *dgc.Router) {
+	r.RegisterCmd(&dgc.Command{
+		Name:        "hello",
+		Description: "Says hello",
+		Usage:       "hello",
+		Example:     "hello",
+		IgnoreCase:  true,
+		Handler: func(ctx *dgc.Ctx) {
+			err := ctx.RespondText("Hello there!")
+			if err != nil {
+				log.Fatalf("hello: %v", err)
+			}
+		},
+	})
+	r.RegisterCmd(&dgc.Command{
+		Name:        "solarian",
+		Description: "get solarian by mint number",
+		Usage:       "solarian",
+		Example:     "solarian 1",
+		IgnoreCase:  true,
+		Handler: func(ctx *dgc.Ctx) {
+			solarian := ctx.Arguments.Get(0).Raw()
+			if len(solarian) == 1 {
+				err := ctx.RespondText("You requested solarian " + solarian)
+				if err != nil {
+					log.Fatalf("solarian: %v", err)
+				}
+			} else if len(solarian) > 1 {
+				err := ctx.RespondText(`https://solarians.click/render/` + solarian)
+				if err != nil {
+					log.Fatalf("solarian: %v", err)
+				}
+			}
+		},
+	})
+}
+
+func main() {
+	session, err := startBot()
+	if err != nil {
+		panic(err)
+	}
+	router := dgc.Create(&dgc.Router{
+		Prefixes: []string{"!"},
+	})
+
+	registerCommands(router)
+
+	router.Initialize(session)
+
+	defer func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+		<-sc
+	}()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/the-robot-factory/solarians-discord-bot
+
+go 1.16
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/lus/dgc v1.1.0
+)
